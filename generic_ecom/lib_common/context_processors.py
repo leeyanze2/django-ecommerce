03453b63ecb65751cfd83615fbcfa1b0e2686b38
Malcolm Lee <leeyanzesg@yahoo.com.sg>
2017-03-11 22:29:46 +0800

    Removing lib_common

--- generic_ecom/lib_common/context_processors.py
+++
@@ -1,7 +0,0 @@
-def base_html(request):
-    from generic_ecom.settings import NV_CLIENT_DETAILS
-    return {
-        'nv_client': {
-            "shop_name": NV_CLIENT_DETAILS.get("SHOP_NAME", "")
-        }
-    }
